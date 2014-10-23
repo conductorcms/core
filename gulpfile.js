4500d762af6e86f3d61098a07f618b64de7ecc4e
Matt Moore <me@mattdoescode.com>
2014-10-23 20:29:08 +0000

    Remove obsolete code from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var gulp = require('gulp')
     watch          = require('gulp-watch')
     templateCache  = require('gulp-angular-templatecache')
     annotate       = require('gulp-ng-annotate')
-	jsonEdit	   = require('gulp-json-editor');
 
 var assets = require('./asset_manifest.json');
 
@@ -20,39 +19,6 @@ gulp.task('list:assets', function()
     listAssets('sass');
     console.log('views:');
     listAssets('views');
-});
-
-// convert asset paths to host paths
-// to allow for gulp tasks to be
-// ran from the host if
-// using Vagrant
-gulp.task('assets:convertPaths', function()
-{
-	var hostPath = __dirname + '/';
-	hostPath = hostPath.substring(0, hostPath.indexOf('\\workbench\\')) + '\\workbench\\';
-	var vagrantPath = assets.js[1].substring(0, assets.js[1].indexOf('/conductor/')) + '/';
-
-	console.log(hostPath);
-	console.log(vagrantPath);
-
-	for(var ii in assets.js)
-	{
-		assets.js[ii] = assets.js[ii].split(vagrantPath).join(hostPath);
-	}
-
-	gulp.src('./asset_manifest.json')
-		.pipe(jsonEdit(function(json)
-		{
-			json.js = assets.js;
-			return json;
-		},
-			{
-				'inden_char': ' ',
-				'indent_size': 4
-			}))
-		.pipe(gulp.dest('./'));
-
-	console.log(assets.js);
 });
 
 gulp.task('build:js', ['build:views'], function()
