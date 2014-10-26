0ebb47b4cfaaf6b3aeb5456c38909a8e546dec84
Matt Moore <me@mattdoescode.com>
2014-10-26 23:44:18 +0000

    Rename test widget to something useful

---
+++ src/Conductor/Core/Widgets/Html/Html.php
@@ -0,0 +1,15 @@
+<?php namespace Conductor\Core\Widgets\Html;
+
+use Conductor\Core\Widget\Widget;
+
+class Html extends Widget {
+
+	public $name = 'HTML';
+	public $description = 'The HTML Widget';
+
+	public $options = [
+		'body' => 'text'
+
+	];
+
+}
